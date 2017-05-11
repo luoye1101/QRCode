9ff323cfe4083fdc9b75bd467d6b5987da94e1fc
huangyueqi <huangyueqi1101@163.com>
2017-05-11 10:31:03 +0800

    创建工程

---
+++ QRCode/QRCode/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  QRCode
+//
+//  Created by huangyueqi on 2017/5/11.
+//  Copyright © 2017年 sjyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ QRCode/QRCode/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  QRCode
+//
+//  Created by huangyueqi on 2017/5/11.
+//  Copyright © 2017年 sjyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
