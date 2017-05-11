b20d6cbd29aead5cd1fe27619e0748e74321aec8
huangyueqi <huangyueqi1101@163.com>
2017-05-11 10:38:22 +0800

    创建 QLQRCodeViewController

---
+++ QRCode/QRCode/QRCode/Classes/Controller/QLQRCodeViewController.h
@@ -0,0 +1,13 @@
+//
+//  QLQRCodeViewController.h
+//  QRCode
+//
+//  Created by huangyueqi on 2017/5/11.
+//  Copyright © 2017年 sjyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface QLQRCodeViewController : UIViewController
+
+@end
---
+++ QRCode/QRCode/QRCode/Classes/Controller/QLQRCodeViewController.m
@@ -0,0 +1,37 @@
+//
+//  QLQRCodeViewController.m
+//  QRCode
+//
+//  Created by huangyueqi on 2017/5/11.
+//  Copyright © 2017年 sjyt. All rights reserved.
+//
+
+#import "QLQRCodeViewController.h"
+
+@interface QLQRCodeViewController ()
+
+@end
+
+@implementation QLQRCodeViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
